7ad9b274a3434afb28cf7920d905b18969c40265
Federico Baldi <federico.baldi@contractors.roche.com>
2022-01-26 14:32:58 +0100

    Fix array manipulation solution

--- problem-solving/arraymanipulation.cpp
+++ problem-solving/arraymanipulation.cpp
@@ -1,18 +1,18 @@
 // https://www.hackerrank.com/challenges/crush
 long arrayManipulation(int n, vector<vector<int>> queries) {
     long max = -1;
-    long arr[n+1];
-    for (long i = 0; i <= n; i++)
+    vector<long> arr(n+1);
+    for (int i = 0; i <= n; i++)
         arr[i] = 0;
-    for (long i = 0; i < queries.size(); i++) {
-        long a = queries[i][0];
-        long b = queries[i][1];
-        long k = queries[i][2];
+    for (int i = 0; i < queries.size(); i++) {
+        int a = queries[i][0];
+        int b = queries[i][1];
+        int k = queries[i][2];
         arr[a-1] += k;
         arr[b] -= k;
     }
     long sum = 0;
-    for(long i=0; i<=n; i++){
+    for(int i=0; i<=n; i++){
         sum += arr[i];
         if(sum > max)
             max = sum;
