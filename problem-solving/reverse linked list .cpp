4703f881192f1497de3ea6c2791cbfba3ec8ce3c
Federico Baldi <federico96.baldi@gmail.com>
2022-02-14 17:16:23 +0100

    Add reverse linked list solution

--- problem-solving/reverse linked list .cpp
+++ problem-solving/reverse linked list .cpp
@@ -1,12 +1,11 @@
-# https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list
-SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
-    SinglyLinkedListNode * previousNode = llist;
-    for (int index = 0; index < position-1; index++) {
-        previousNode = previousNode->next;
+# https://www.hackerrank.com/challenges/reverse-a-linked-list
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* llist) {
+    SinglyLinkedListNode * headReverse = NULL;
+    while (llist != NULL) {
+        SinglyLinkedListNode * currentHead = headReverse;
+        headReverse = new SinglyLinkedListNode(llist->data);
+        headReverse->next = currentHead;
+        llist = llist->next;
     }
-    SinglyLinkedListNode *nextNode = previousNode->next;
-    SinglyLinkedListNode *node = new SinglyLinkedListNode(data);
-    previousNode->next = node;
-    node->next = nextNode;
-    return llist;
+    return headReverse;
 }
